ee352d52f5e527f9ffb86c02cf605c117ca4a17e
Robin9800 <robin.costanzo@gmail.com>
2022-03-28 17:42:29 +0200

    Exercise completed

---
+++ script.js
@@ -0,0 +1,15 @@
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+//Bonus: aggiungere alla pagina un’immagine, presa anch’essa da un data. 
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: 'Armored Titan',
+            mieClassi: 'color',
+            immagine: 'https://i.redd.it/fofrhdw84m361.jpg'
+        },
+        
+    }
+    
+);
